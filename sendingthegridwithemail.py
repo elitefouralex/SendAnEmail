d614039f2c1ea8ec9a640702b4f27ad7a7cb0b2b
Alex Oliveira <58787277+elitefouralex@users.noreply.github.com>
2021-01-04 21:46:28 -0800

    Add files via upload

---
+++ sendingthegridwithemail.py
@@ -0,0 +1,23 @@
+
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+destination_email=input("\nWhat email address would you like to send a message to?\n>")
+email_subject=input("\nDefine the subject.\n>")
+email_content=input("\nWhat would you like in the body of the email?\n>")
+
+message = Mail(
+    from_email= os.environ.get("my_email"),
+    to_emails= destination_email,
+    subject= email_subject,
+    html_content= email_content)
+try:
+    sg = SendGridAPIClient(os.environ.get('sendgrid_key'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+    print("\nEmail Sent")
+except Exception as e:
+    print(e)
